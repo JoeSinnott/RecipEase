cb344ec24855660ec696b0afea88d25ef3c2d9a7
Aqil Patel <aqilpatel06@gmail.com>
2025-02-11 15:02:23 +0000

    added in fading in and out animations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import RecipesPage from "./pages/RecipesPage";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import ForgotPassword from "./pages/ForgotPassword";  // Import ForgotPassword
+import ForgotPassword from "./pages/ForgotPassword";
 import "./styles.css";
+
+const PageTransitionWrapper = ({ children }) => {
+  const location = useLocation();
+  const [showPage, setShowPage] = useState(false);
+
+  useEffect(() => {
+    setShowPage(false);
+    setTimeout(() => setShowPage(true), 300); // Short delay to fade in new page
+  }, [location]);
+
+  return <div className={`page-transition ${showPage ? "fade-in" : "fade-out"}`}>{children}</div>;
+};
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/recipes" element={<RecipesPage />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} /> {/* Add this line */}
-      </Routes>
+      <PageTransitionWrapper>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recipes" element={<RecipesPage />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </PageTransitionWrapper>
       <Footer />
     </Router>
   );
