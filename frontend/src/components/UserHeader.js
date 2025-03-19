a81b979a0afd2f01fbb94a96e7522139e3d5d222
Navjot Gill <navjot.gill@student.manchester.ac.uk>
2025-03-19 20:24:13 +0000

    new header with username and signout button, to be styled better

---
+++ frontend/src/components/UserHeader.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const UserHeader = ({user, onSignOut}) => {
+  return (
+    <header>
+      <div className="container">
+        <div className="logo">
+          <h1>RecipEase</h1>
+        </div>
+        <nav>
+          <ul className="nav-links">
+            <span>Welcome, {user.username}!</span>
+            <li><Link to="/userhomepage">Home</Link></li>
+            <li><Link to="/recipes">Recipes</Link></li>
+            <li><Link to="/favorites">Favorites</Link></li>
+            <li><Link to="/create-recipe">Create Recipe</Link></li>
+            <li><Link to="/settings">Settings</Link></li>
+            <li><button className="nav-button" onClick={onSignOut}>Sign Out</button></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default UserHeader;
