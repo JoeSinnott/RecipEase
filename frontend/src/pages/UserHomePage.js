1bb6918185df4985f027202735d9c64c3e1a4eff
Navjot Gill <navjot.gill@student.manchester.ac.uk>
2025-03-19 20:22:25 +0000

    new page for signed in users

---
+++ frontend/src/pages/UserHomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+
+const UserHomePage = () => {
+  return (
+    <main>
+      <Hero />
+      <Features />
+    </main>
+  );
+};
+
+export default UserHomePage;
